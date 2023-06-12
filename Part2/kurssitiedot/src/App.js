461984c074d107deb38677f9524114b9d612078f
Markus Paananen <markus.anton@gmail.com>
2023-06-12 14:24:14 +0300

    2.3*: Course information step8

--- Part2/kurssitiedot/src/App.js
+++ Part2/kurssitiedot/src/App.js
@@ -2,7 +2,9 @@ const Header = ({ course }) => <h1>{course}</h1>;
 
 const Total = ({ parts }) => {
   const totalExercises =
-    parts.reduce((sum, part) => sum + part.exercises, 0);
+    parts.reduce(function (sum, part) {
+      return sum + part.exercises;
+    }, 0);
   return (
     <div>
       <p>Total of {totalExercises} exercises</p>
