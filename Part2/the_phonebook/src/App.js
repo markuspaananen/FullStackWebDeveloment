b48eafb0cd1fc2853b9a4f97ce7d8057667800a6
Markus Paananen <markus.anton@gmail.com>
2023-06-28 15:01:34 +0300

    2.7: The Phonebook Step2

--- Part2/the_phonebook/src/App.js
+++ Part2/the_phonebook/src/App.js
@@ -11,41 +11,46 @@ const App = (props) => {
   // Event handler
   const addName = (event) => {
     event.preventDefault()
-    const nameObject = {
-      name: newName,
-      id: persons.length + 1,
+    if (persons.find((person) => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`)
+    }
+    else {
+      const nameObject = {
+        name: newName,
+        id: persons.length + 1,
+      }
+      setPersons(persons.concat(nameObject))
+      setNewName('')
     }
 
-    setPersons(persons.concat(nameObject))
-    setNewName('')
-  };
+    };
 
-  const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  };
+    const handleNameChange = (event) => {
+      setNewName(event.target.value)
+    };
 
-  // JSX Markup inside return()
-  return (
-    <div>
-      <h2>Phonebook</h2>
+    // JSX Markup inside return()
+    return (
+      <div>
+        <h2>Phonebook</h2>
 
-      <form onSubmit={addName} >
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      <ul>
-        {persons.map((person) => (
-          <li key={person.id}>{person.name}</li>
-        ))}
-      </ul>
-    </div>
-  )
-}
+        <form onSubmit={addName} >
+          <div>
+            name: <input value={newName} onChange={handleNameChange} />
+          </div>
+          <div>
+            <button type="submit">add</button>
+          </div>
+        </form>
+        <h2>Numbers</h2>
+        <ul>
+          {persons.map((person) => (
+            <li key={person.id}>{person.name}</li>
+          ))}
+        </ul>
+      </div>
+    )
+  }
 
-//Export statement
-export default App
+  //Export statement
+  export default App
